dd2dec5283d2850d363cb0e419aa453696bfe125
Denilson <denilson2020@alu.ufc.br>
2022-06-08 17:03:47 -0300

    Primeiro commit

---
+++ servidor.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const app = express();
+var cors = require('cors')
+var port = process.env.PORT || 3000
+dotenv.config()
+
+
+app.use(cors())
+app.use(express.json());
+mongoose.connect(`${process.env.MONGOOSE_URL}`)
+require('./routes/routes.js')(app)
+app.listen(port)
